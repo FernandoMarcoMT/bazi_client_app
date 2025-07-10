7af6daee1fc228e8990c00adf0862c7efb2f026f
Nama Kamu <marco3072003@gmail.com>
2025-07-10 21:55:06 +0700

    fix error type userdata for vercel

--- src/app/api/submit-user.ts
+++ src/app/api/submit-user.ts
@@ -1,10 +1,12 @@
 // pages/api/submit-user.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { UserData } from '../../types/user';
+// import { UserData } from '../../types/user';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
-        const userData: UserData = req.body;
+        // const userData: UserData = req.body;
+        const userData: any = req.body;
+
 
         // Here, you can process the data (e.g., save to a database)
         console.log('Received user data:', userData);

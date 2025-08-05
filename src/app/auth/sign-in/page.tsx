b0a36a97ffb2eb70701df93c3f2220e6a070d9e5
70002007 <ardiansyahputra@steradian.co.id>
2025-08-05 15:48:02 +0700

    fix(auth): content sign in page

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 
+import ButtonShaped from "@/components/button-shaped";
 import { Input } from "@/components/ui/input";
+import {
+  IconBrandAppleFilled,
+  IconBrandGoogle,
+  IconBrandGoogleFilled,
+} from "@tabler/icons-react";
 import { ChevronLeft } from "lucide-react";
 import Link from "next/link";
 import { FormEvent, useState } from "react";
@@ -17,7 +23,7 @@ export default function SignIn() {
 
   return (
     <div className="relative flex flex-col overflow-hidden">
-      <div className="absolute -top-26 h-full w-full">
+      <div className="hidden md:block absolute -top-26 h-full w-full">
         <img
           src="/images/bg-auth.png"
           alt="Background Auth"
@@ -25,24 +31,27 @@ export default function SignIn() {
         />
       </div>
 
-      <div className="flex flex-col h-full px-[24px] xl:px-14 2xl:px-40 pt-10 xl:pt-20 pb-10 gap-14 xl:gap-4 justify-between z-[1]">
+      <div className="flex flex-col h-[calc(100vh-64px)] md:h-full px-[24px] xl:px-14 2xl:px-40 pt-10 xl:pt-20 pb-10 gap-14 xl:gap-4 justify-between z-[1]">
         <Link href="/" className="hidden xl:flex items-center gap-2 w-fit">
           <ChevronLeft className="size-10" />
           <p className="font-oswald uppercase font-bold text-3xl">Back</p>
         </Link>
 
-        <div className="border border-secondary bg-[#F4FAFF] p-10 rounded-md w-full max-w-[500px] mx-auto">
-          <div className="flex flex-col gap-2">
-            <p className="uppercase text-3xl font-bold font-oswald text-black">
+        <div className="xl:border border-secondary md:bg-[#F4FAFF] md:p-9 rounded-md w-full max-w-[500px] mx-auto h-full">
+          <div className="flex flex-col gap-3 md:gap-2 h-full">
+            <p className="uppercase text-3xl font-bold font-oswald text-white md:text-black">
               Login
             </p>
-            <p className="text-lg text-black">
+            <p className="text-base md:text-lg text-white md:text-black font-light md:font-normal">
               Enter your login details to continue.
             </p>
 
             <form onSubmit={handleSubmit} className="flex flex-col gap-4 mt-2">
               <div className="flex flex-col gap-1">
-                <label htmlFor="email" className="font-semibold text-black">
+                <label
+                  htmlFor="email"
+                  className="font-semibold text-white md:text-black"
+                >
                   Email Address <span className="text-red-500">*</span>
                 </label>
                 <Input
@@ -58,8 +67,12 @@ export default function SignIn() {
                   className="h-12 bg-[#F4FAFF]! xl:bg-[#A8A8A8]/10! border-[#898888] text-black text-sm! xl:text-base! focus-within:bg-[#F6F6F0]! focus-within:border-secondary! focus-visible:ring-0 transition-all duration-200"
                 />
               </div>
+
               <div className="flex flex-col gap-1">
-                <label htmlFor="password" className="font-semibold text-black">
+                <label
+                  htmlFor="password"
+                  className="font-semibold text-white md:text-black"
+                >
                   Password <span className="text-red-500">*</span>
                 </label>
                 <Input
@@ -69,14 +82,54 @@ export default function SignIn() {
                   onChange={(e) =>
                     setInputValue((prevVal) => ({
                       ...prevVal,
-                      email: e.target.value,
+                      password: e.target.value,
                     }))
                   }
                   className="h-12 bg-[#F4FAFF]! xl:bg-[#A8A8A8]/10! border-[#898888] text-black text-sm! xl:text-base! focus-within:bg-[#F6F6F0]! focus-within:border-secondary! focus-visible:ring-0 transition-all duration-200"
                 />
-                <p className="text-right text-black text-sm">Forget Password?</p>
+                <p className="text-right text-white xl:text-black text-sm">
+                  Forget Password?
+                </p>
               </div>
+
+              <ButtonShaped
+                text="Log In"
+                variant="light"
+                buttonClassName="w-full mt-2 hidden sm:flex"
+                width={426}
+                disabled={!inputValue.email || !inputValue.password}
+              />
+
+              <ButtonShaped
+                text="Log In"
+                variant="light"
+                buttonClassName="w-full mt-2 flex sm:hidden"
+                width={426}
+                height={70}
+                disabled={!inputValue.email || !inputValue.password}
+              />
             </form>
+
+            <div className="flex flex-col w-full items-center gap-6 mt-4">
+              <p className="text-lg text-white md:text-black">or</p>
+
+              <div className="flex items-center gap-6">
+                <div className="flex items-center justify-center w-13 h-13 border border-[#8E8E8E] rounded-full">
+                  <IconBrandGoogleFilled className="text-white md:text-black" />
+                </div>
+                <div className="flex items-center justify-center w-13 h-13 border border-[#8E8E8E] rounded-full">
+                  <IconBrandAppleFilled className="text-white md:text-black" />
+                </div>
+              </div>
+            </div>
+
+            <div className="flex justify-center items-center gap-1.5 text-white md:text-black text-lg mt-auto md:mt-10">
+              <p>No account yet?</p>
+              <Link href="/auth/sign-up" className="font-semibold">
+                Sign up
+              </Link>
+              <p>here</p>
+            </div>
           </div>
         </div>
       </div>

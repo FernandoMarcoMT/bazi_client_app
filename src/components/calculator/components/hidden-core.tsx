dfd0d5a971ce02120c96e84a146257b58150c156
Ardi <ardihp8@gmail.com>
2025-08-17 17:27:52 +0700

    fix(components): calculation section hidden core

--- src/components/calculator/components/hidden-core.tsx
+++ src/components/calculator/components/hidden-core.tsx
@@ -7,48 +7,120 @@ export default function HiddenCoreSection() {
         Hidden Core Personality
       </p>
       <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-4 xl:gap-8 h-full md:min-h-[400px] 2xl:min-h-[500px]">
-        {[...new Array(3)].map((item, key) => (
-          <div
-            key={key}
-            className="flex flex-col justify-center gap-6 p-8 md:p-10 lg:p-12 2xl:p-15 relative border-[3px] border-collapse border-secondary"
-          >
-            <img
-              src="/images/Hidden Core Personality/left-top.svg"
-              alt="Border Top Left"
-              className="absolute top-[-3px] left-[-3px] max-w-[30%]"
-            />
-            <img
-              src="/images/Hidden Core Personality/right-top.svg"
-              alt="Border Top Right"
-              className="absolute top-[-3px] right-[-3px] max-w-[30%]"
-            />
-            <img
-              src="/images/Hidden Core Personality/left-bottom.svg"
-              alt="Border Bottom Left"
-              className="absolute bottom-[-3px] left-[-3px] max-w-[30%]"
-            />
-            <img
-              src="/images/Hidden Core Personality/right-bottom.svg"
-              alt="Border Bottom Right"
-              className="absolute bottom-[-3px] right-[-3px] max-w-[30%]"
-            />
+        <div className="flex flex-col justify-center gap-6 p-8 md:p-10 lg:p-12 2xl:p-15 relative border-[3px] border-collapse border-secondary">
+          <img
+            src="/images/Hidden Core Personality/left-top.svg"
+            alt="Border Top Left"
+            className="absolute top-[-3px] left-[-3px] max-w-[30%]"
+          />
+          <img
+            src="/images/Hidden Core Personality/right-top.svg"
+            alt="Border Top Right"
+            className="absolute top-[-3px] right-[-3px] max-w-[30%]"
+          />
+          <img
+            src="/images/Hidden Core Personality/left-bottom.svg"
+            alt="Border Bottom Left"
+            className="absolute bottom-[-3px] left-[-3px] max-w-[30%]"
+          />
+          <img
+            src="/images/Hidden Core Personality/right-bottom.svg"
+            alt="Border Bottom Right"
+            className="absolute bottom-[-3px] right-[-3px] max-w-[30%]"
+          />
 
-            <img
-              src="/images/Hidden Core Personality/Element/fire.png"
-              alt="Element"
-              className="w-full max-w-[120px] md:max-w-[180px] xl:max-w-[220px] object-cover object-center mx-auto"
-            />
-            <div className="flex flex-col gap-2 md:gap-3">
-              <p className="font-oswald font-bold text-xl md:text-3xl uppercase">
-                Bing Earth
-              </p>
-              <p className="font-light md:font-normal text-sm md:text-base 2xl:text-xl">
-                The sacred mountain. Still, eternal, and wise. Through chaos,
-                their silence echoes that all things shall pass.
-              </p>
-            </div>
+          <img
+            src="/images/Hidden Core Personality/Element/fire.png"
+            alt="Element"
+            className="w-full max-w-[120px] md:max-w-[180px] xl:max-w-[220px] object-cover object-center mx-auto"
+          />
+          <div className="flex flex-col gap-2 md:gap-3">
+            <p className="font-oswald font-bold text-xl md:text-3xl uppercase">
+              Bing Earth
+            </p>
+            <p className="font-light md:font-normal text-sm md:text-base 2xl:text-xl">
+              The sacred mountain. Still, eternal, and wise. Through chaos,
+              their silence echoes that all things shall pass.
+            </p>
           </div>
-        ))}
+        </div>
+
+        <div className="flex flex-col justify-center gap-6 p-8 md:p-10 lg:p-12 2xl:p-15 relative border-[3px] border-collapse border-secondary">
+          <img
+            src="/images/Hidden Core Personality/left-top.svg"
+            alt="Border Top Left"
+            className="absolute top-[-3px] left-[-3px] max-w-[30%]"
+          />
+          <img
+            src="/images/Hidden Core Personality/right-top.svg"
+            alt="Border Top Right"
+            className="absolute top-[-3px] right-[-3px] max-w-[30%]"
+          />
+          <img
+            src="/images/Hidden Core Personality/left-bottom.svg"
+            alt="Border Bottom Left"
+            className="absolute bottom-[-3px] left-[-3px] max-w-[30%]"
+          />
+          <img
+            src="/images/Hidden Core Personality/right-bottom.svg"
+            alt="Border Bottom Right"
+            className="absolute bottom-[-3px] right-[-3px] max-w-[30%]"
+          />
+
+          <img
+            src="/images/Hidden Core Personality/Zodiac/rat.png"
+            alt="Zodiac"
+            className="w-full max-w-[120px] md:max-w-[180px] xl:max-w-[220px] object-cover object-center mx-auto"
+          />
+          <div className="flex flex-col gap-2 md:gap-3">
+            <p className="font-oswald font-bold text-xl md:text-3xl uppercase">
+              Bing Fire Rat
+            </p>
+            <p className="font-light md:font-normal text-sm md:text-base 2xl:text-xl">
+              Clever yet grounded. Strategic and calm, they move with quiet
+              purpose, blending instinct with steady ambition.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-col justify-center gap-6 p-8 md:p-10 lg:p-12 2xl:p-15 relative border-[3px] border-collapse border-secondary">
+          <img
+            src="/images/Hidden Core Personality/left-top.svg"
+            alt="Border Top Left"
+            className="absolute top-[-3px] left-[-3px] max-w-[30%]"
+          />
+          <img
+            src="/images/Hidden Core Personality/right-top.svg"
+            alt="Border Top Right"
+            className="absolute top-[-3px] right-[-3px] max-w-[30%]"
+          />
+          <img
+            src="/images/Hidden Core Personality/left-bottom.svg"
+            alt="Border Bottom Left"
+            className="absolute bottom-[-3px] left-[-3px] max-w-[30%]"
+          />
+          <img
+            src="/images/Hidden Core Personality/right-bottom.svg"
+            alt="Border Bottom Right"
+            className="absolute bottom-[-3px] right-[-3px] max-w-[30%]"
+          />
+
+          <img
+            src="/images/Hidden Core Personality/God/eating-god.png"
+            alt="God"
+            className="w-full max-w-[120px] md:max-w-[180px] xl:max-w-[220px] object-cover object-center mx-auto"
+          />
+          <div className="flex flex-col gap-2 md:gap-3">
+            <p className="font-oswald font-bold text-xl md:text-3xl uppercase">
+              Eating God
+            </p>
+            <p className="font-light md:font-normal text-sm md:text-base 2xl:text-xl">
+              The child of your essence, a muse born of soul and spirit. It
+              weaves beauty from breath, nurturing the world with quiet magic
+              and effortless grace.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
